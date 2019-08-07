a13df6f81ad6f98db99c81a403d1bb55ade7da6e
Jeremy <jeremy.lorelli.1337@gmail.com>
2019-08-07 13:16:56 -0700

    Update test materials

--- iocBoot/iocEK9KTest/st.cmd
+++ iocBoot/iocEK9KTest/st.cmd
@@ -11,7 +11,7 @@ ek9000Test_registerRecordDeviceDriver pdbbase
 
 # Configure the device
 ek9000Configure("EK9K1", "192.168.1.3", 502, 7)
-ek9000ConfigureTerminal("EK9K1", "TestTerm1-AO", "EL4004", 1)
+ek9000ConfigureTerminal("EK9K1", "TestTerm1-BI", "EL1004", 1)
 ek9000ConfigureTerminal("EK9K1", "TestTerm2-AI", "EL3064", 2)
 ek9000ConfigureTerminal("EK9K1", "TestTerm3-BO", "EL2008", 3)
 ek9000ConfigureTerminal("EK9K1", "TestTerm4-AI", "EL3054", 4)
@@ -20,7 +20,7 @@ ek9000EnableDebug("EK9K1")
 
 cd "${TOP}/src/ek9000/iocBoot/${IOC}"
 
-dbLoadTemplate("EL4004.substitutions")
+#dbLoadTemplate("EL4004.substitutions")
 dbLoadTemplate("EL2008.substitutions")
 dbLoadTemplate("EL3064.substitutions")
 dbLoadTemplate("EL3054.substitutions")
