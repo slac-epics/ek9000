e608dcb99082c19ffb4a51bbb554c1b331f5c336
jjl77 <jeremy.lorelli.1337@gmail.com>
2020-01-23 12:56:12 -0800

    More code cleanup

--- ek9000App/src/devEL7XXX.h
+++ ek9000App/src/devEL7XXX.h
@@ -31,77 +31,6 @@
 /* RxPDOs : 0x1601 0x1602 0x1605 (things written to terminal by epics) */
 /* TxPDOs: 0x1A01 0x1A03 0x1A06 (things read from terminal by epics) */
 #pragma pack(1)
-struct SPositionInterfaceCompact_Output
-{
-	/* 0x1601 */
-	uint8_t enc_enable_lat_c : 1;	/* Enable latching thru the C track */
-	uint8_t enc_enable_lat_epe : 1; /* extern on positive edge */
-	uint8_t enc_set_counter : 1;	/* Set the counter value */
-	uint8_t enc_enable_lat_ene : 1; /* extern on negative edge */
-	uint16_t _r4 : 12;
-	uint32_t enc_counter_val;	/* The value to be set thru set counter */
-
-	/* 0x1602:0x7010 */
-	uint8_t stm_enable	: 1;			/* Enable output stage */
-	uint8_t stm_reset	: 1;			/* Reset and clear all errors */
-	uint8_t stm_reduce_torque : 1;		/* Recuded coil current is active */
-	uint8_t _r2 : 8;
-	uint8_t stm_digout1 : 1;		/* Digital output 1 */
-	uint8_t _r3 : 4;
-
-	/* 0x1605:0x7020 */
-	uint8_t pos_execute : 1;		/* Begin move with settings */
-	uint8_t pos_emergency_stp : 1;	/* Emergency stop */
-	uint16_t _r1 : 14;
-	uint32_t pos_tgt_pos;			/* Target position */
-};
-
-struct SPositionInterfaceCompact_Input
-{
-	/* 0x1A01, 0x6000 */
-	uint32_t latc_valid : 1; /* Latch c valid */
-	uint32_t latc_ext_valid : 1; /* Latch C extern valid */
-	uint32_t cntr_done : 1; /* The counter was set */
-	uint32_t cntr_underflow : 1;
-	uint32_t cntr_overflow : 1;
-	uint32_t _r1 : 2;
-	uint32_t extrap_stall : 1; /* Extrapolated part of the counter was invalid */
-	uint32_t stat_inp_a : 1; /* status of input a */
-	uint32_t stat_inp_b : 1; /* status of inp b */
-	uint32_t stat_inp_c : 1; /* Status of inp c */
-	uint32_t _r2 : 1; /* 2 bits or one??? */
-	uint32_t stat_ext_lat : 1; /* Status of extern latch */
-	uint32_t sync_err : 1;	/* Sync error */
-	uint32_t _r3 : 1;
-	uint32_t txpdo_toggle : 1; /* Toggled when data is updated */
-	uint32_t cntr_val; /* The counter value */
-	uint32_t lat_val; /* Latch value */
-	/* 0x1A03, 0x6010 */
-	uint32_t stm_rdy_enable : 1; /* Driver stage is ready for enabling */
-	uint32_t stm_rdy : 1; /* Driver stage is ready for operation */
-	uint32_t stm_warn : 1; /* warning has happened */
-	uint32_t stm_err : 1; /* Error has happened */
-	uint32_t stm_mov_pos : 1; /* Moving in the positive dir */
-	uint32_t stm_mov_neg : 1; /* Moving in the negative dir */
-	uint32_t stm_tor_reduce : 1; /* Reduced torque */
-	uint32_t stm_stall : 1;		 /* Motor stall */
-	uint32_t _r4 : 3;
-	uint32_t stm_sync_err : 1; /* Set if synchronization error in previous step */
-	uint32_t _r5 : 1;
-	uint32_t stm_txpdo_toggle : 1; /* txpdo toggle  */
-	uint32_t _r6 : 1;
-	uint32_t stm_txpdo_toggle2 : 1;
-	/* 0x1A06, 0x6020 */
-	uint32_t stat_busy : 1; /* current travel command is active */
-	uint32_t stat_in_tgt : 1; /* Motor has arrived */
-	uint32_t stat_warn : 1; /* Warning happened */
-	uint32_t stat_err : 1; /* Error happened */
-	uint32_t stat_calibrated : 1; /* Motor is calibrated */
-	uint32_t stat_accel : 1; /* Motor is accelerating */
-	uint32_t stat_deaccel : 1; /* Motor is slowing down */
-	uint32_t _r7 : 9;
-};
-
 struct SPositionInterface_Output
 {
 	/* 0x1601 */
@@ -193,14 +122,6 @@ class EL70X7Axis
 class epicsShareClass el70x7Axis : public asynMotorAxis
 {
 public:
-	class CouplerLock
-	{
-		el70x7Axis* axis;
-	public:
-		CouplerLock(el70x7Axis* axis);
-
-		~CouplerLock();
-	};
 
 	CEK9000Device* pcoupler;
 	CTerminal* pcontroller;
