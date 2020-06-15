25d43e4b7968c312af5883838f79526b3f966fc9
Jeremy Lorelli <jeremy.lorelli.1337@gmail.com>
2020-06-15 09:00:59 -0700

    Pull in changes from private repo

--- ek9000App/src/devEL3XXX.cpp
+++ ek9000App/src/devEL3XXX.cpp
@@ -1,249 +1,97 @@
 /*
- * This file is part of the EK9000 device support module. It is subject to 
- * the license terms in the LICENSE.txt file found in the top-level directory 
- * of this distribution and at: 
- *    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html. 
- * No part of the EK9000 device support module, including this file, may be 
- * copied, modified, propagated, or distributed except according to the terms 
- * contained in the LICENSE.txt file.
-*/
-//======================================================//
-// Name: devEL3XXX.cpp
-// Purpose: Device support for EL3xxx modules (analog in)
-// Authors: Jeremy L.
-// Date Created: July 6, 2019
-//======================================================//
+ *
+ * devEL3XXX.cpp
+ *
+ * Analog input terminals
+ *
+ */ 
 
-/* EPICS includes */
-#include <epicsExport.h>
-#include <epicsMath.h>
-#include <epicsStdio.h>
-#include <epicsStdlib.h>
-#include <epicsAssert.h>
-#include <dbAccess.h>
-#include <devSup.h>
-#include <alarm.h>
-#include <epicsString.h>
-#include <dbScan.h>
 #include <aiRecord.h>
-#include <iocsh.h>
-#include <callback.h>
-#include <atomic>
+#include <epicsSpin.h>
+#include <epicsStdio.h>
 
-#include <drvModbusAsyn.h>
-#include <asynPortDriver.h>
+#include "util.h"
+#include "drvEK9000.h"
 
-#include <stddef.h>
-#include <stdint.h>
-#include <string.h>
+static long el30xx_read_record(void* precord);
+static long el30xx_init_record(void* precord);
+static long el33xx_read_record(void* precord);
+static long el33xx_init_record(void* precord);
 
-#include "devEK9000.h"
-
-//======================================================//
-// 
-//	EL30XX Device support
-//
-//======================================================//
-static void EL30XX_ReadCallback(CALLBACK* callback);
-static long	EL30XX_dev_report(int interest);
-static long EL30XX_init(int after);
-static long EL30XX_init_record(void *precord);
-static long EL30XX_read_record(void *precord);
-static long EL30XX_linconv(void* precord, int after);
-
-struct
-{
-	long number;
-	DEVSUPFUN dev_report;
-	DEVSUPFUN init;
-	DEVSUPFUN init_record;
-	DEVSUPFUN get_ioint_info;
-	DEVSUPFUN read_record;
-	DEVSUPFUN linconv;
+/*
+ * Device support for basic analog input records with 
+ * 32-bits per channel in standard mode, 16-bit status, 
+ * 16-bit adc count.
+ * this also supports compact PDO mode, with 16-bits per channel,
+ * no status, only adc counts
+ */ 
+struct {
+	long        number;
+	DEVSUPFUN   report;
+	DEVSUPFUN   init;
+	DEVSUPFUN   init_record;
+	DEVSUPFUN   get_ioint_info;
+	DEVSUPFUN   read_ai;
+	DEVSUPFUN   special_linconv;
 } devEL30XX = {
 	6,
-	(DEVSUPFUN)EL30XX_dev_report,
-	(DEVSUPFUN)EL30XX_init,
-	(DEVSUPFUN)EL30XX_init_record,
 	NULL,
-	(DEVSUPFUN)EL30XX_read_record,
-	(DEVSUPFUN)EL30XX_linconv,
-};
-
-epicsExportAddress(dset, devEL30XX);
-
-struct SEL30XXSupportData
-{
-	CTerminal* m_pTerminal;
-	CEK9000Device* m_pDevice;
-	int m_nChannel;
-	/* Compact or standard PDO used */
-	bool m_bCompactPDO;
-	/* PDO type, 1 = standard, 2 = compact */
-	int m_nPdoType;
-};
-
-static void EL30XX_ReadCallback(CALLBACK* callback)
-{
-	void* record;
-	callbackGetUser(record, callback);
-	aiRecord* pRecord = (aiRecord*)record;
-	SEL30XXSupportData* dpvt = (SEL30XXSupportData*)pRecord->dpvt;
-	free(callback);
-
-	/* Check for invalid */
-	if(!dpvt->m_pTerminal)
-		return;
-
-	/* Lock mutex */
-	int status = dpvt->m_pTerminal->m_pDevice->Lock();
-
-	if(status != epicsMutexLockOK)
-	{
-		DevError("EL30XX_ReadCallback(): %s\n", CEK9000Device::ErrorToString(EK_EMUTEXTIMEOUT));
-		return;
-	}
-	uint16_t buf[2];
-	if(dpvt->m_nPdoType == EL30XX_STANDARD_ID)
-	{
-		/* Standard analog is 32-bits per channel, low word is adc value, high word is some info stuff */
-		/* Some note on this: m_nChannel is treated as an offset, which is why we do m_nChannel-1. Likewise, modbus registers are 16 bits wide,
-		 * and we're trying to read 4 bytes total (because the standard pdo mapping defines some status data in the high byte. This is why:
-		 * m_nChannel-1 * 2. This computes the register offset. */
-		status = dpvt->m_pTerminal->doEK9000IO(MODBUS_READ_INPUT_REGISTERS, dpvt->m_pTerminal->m_nInputStart+((dpvt->m_nChannel-1)*2), buf, 2);
-	}
-	/* In the case of compact pdo */
-	else
-	{
-		/* Compact analog is 16-bits per channel, completely adc value */
-		/* Some note on this: we only need to read 16-bits here, so m_nChannel just represents the register offset */
-		status = dpvt->m_pTerminal->doEK9000IO(MODBUS_READ_INPUT_REGISTERS, dpvt->m_pTerminal->m_nInputStart + ((dpvt->m_nChannel)-1), buf, 1);
-	}
-	/* Set props */
-	pRecord->rval = buf[0];
-	pRecord->pact = 0;
-	dpvt->m_pTerminal->m_pDevice->Unlock();
-
-	/* Check for error */
-	if(status)
-	{
-		if(status > 0x100)
-		{
-			DevError("EL30XX_ReadCallback(): %s\n", CEK9000Device::ErrorToString(EK_EMODBUSERR));
-			return;
-		}
-		DevError("EL30XX_ReadCallback(): %s\n", CEK9000Device::ErrorToString(status));
-		return;
-	}
-	return;
-}
-
-static long EL30XX_dev_report(int interest)
-{
-	return 0;
-}
-
-static long EL30XX_init(int after)
-{
-	return 0;
-}
-
-static long EL30XX_init_record(void *precord)
-{
-	aiRecord* pRecord = (aiRecord*)precord;
-	pRecord->dpvt = calloc(1, sizeof(SEL30XXSupportData));
-	SEL30XXSupportData* dpvt = (SEL30XXSupportData*)pRecord->dpvt;
-
-	/* Get the terminal */
-	char* recname = NULL;
-	dpvt->m_pTerminal = CTerminal::ProcessRecordName(pRecord->name, dpvt->m_nChannel, recname);
-	if(!dpvt->m_pTerminal)
-	{
-		Error("EL30XX_init_record(): Unable to find terminal for record %s\n", pRecord->name);
-		return 1;
-	}
-	free(recname);
-	
-	dpvt->m_nPdoType = dpvt->m_pTerminal->m_nPdoID;
-	dpvt->m_pDevice = dpvt->m_pTerminal->m_pDevice;
-	dpvt->m_pDevice->Lock();
-
-	/* Check connection to terminal */
-	if(!dpvt->m_pTerminal->m_pDevice->VerifyConnection())
-	{
-		Error("EL30XX_init_record(): %s\n", CEK9000Device::ErrorToString(EK_ENOCONN));
-		dpvt->m_pDevice->Unlock();
-		return 1;
-	}
-
-	/* Check that slave # is OK */
-	uint16_t termid = 0;
-	dpvt->m_pTerminal->m_pDevice->ReadTerminalID(dpvt->m_pTerminal->m_nTerminalIndex, termid);
-	dpvt->m_pDevice->Unlock();
-
-	if(termid != dpvt->m_pTerminal->m_nTerminalID || termid == 0)
-	{
-		Error("EL30XX_init_record(): %s: %s != %u\n", CEK9000Device::ErrorToString(EK_ETERMIDMIS), pRecord->name, termid);
-		return 1;
-	}
-	return 0;
-}
-
-static long EL30XX_read_record(void *precord)
-{
-	aiRecord* pRecord = (aiRecord*)precord;
-	/* indicate processing */
-	pRecord->pact = 1;
-	/* Allocate and set callback */
-	CALLBACK *callback = (CALLBACK *)malloc(sizeof(CALLBACK));
-	*callback = *(CALLBACK*)EL30XX_ReadCallback;
-	callbackSetUser(pRecord, callback);
-	callbackSetCallback(EL30XX_ReadCallback, callback);
-	callbackSetPriority(priorityHigh, callback);
-	callbackRequest(callback);
-
-	return 0;
-}
-
-/* EL30XX uses a 12-bit ADC value that's sign extended to 16-bits for transmission */
-static long EL30XX_linconv(void* precord, int after)
-{
-	aiRecord* pRecord = (aiRecord*)precord;
-	/* Max range is 32767 */
-	pRecord->eslo = (pRecord->eguf - pRecord->egul) / 32767;
-	pRecord->roff = 0x0;
-	return 0;
-}
-
-static long EL31XX_linconv(void* precord, int after);
-
-struct
-{
-	long number;
-	DEVSUPFUN dev_report;
-	DEVSUPFUN init;
-	DEVSUPFUN init_record;
-	DEVSUPFUN get_ioint_info;
-	DEVSUPFUN read_record;
-	DEVSUPFUN linconv;
-} devEL31XX = {
-	6,
-	(DEVSUPFUN)EL30XX_dev_report,
-	(DEVSUPFUN)EL30XX_init,
-	(DEVSUPFUN)EL30XX_init_record,
 	NULL,
-	(DEVSUPFUN)EL30XX_read_record,
-	(DEVSUPFUN)EL31XX_linconv,
+	el30xx_init_record,
+	NULL,
+	el30xx_read_record,
+	NULL
 };
 
-epicsExportAddress(dset, devEL31XX);
+/*
+ * Device support for EL33XX thermocouple modules
+ * These have a total of 48-bits per channel
+ */ 
+struct {
+    long        number;
+    DEVSUPFUN   report;
+    DEVSUPFUN   init;
+    DEVSUPFUN   init_record;
+    DEVSUPFUN   get_ioint_info;
+    DEVSUPFUN   read_ai;
+    DEVSUPFUN   special_linconv;
+} devEL33XX = {
+    6,
+    NULL,
+    NULL,
+    el33xx_init_record,
+    NULL,
+    el33xx_read_record,
+    NULL
+};
 
-/* Nearly identical to the EL30XX stuff, except EL1XX uses 16-bit unsigned ints for each channel */
-static long EL31XX_linconv(void* precord, int after)
+static long el30xx_init_record(void* precord)
 {
-	aiRecord* pRecord = (aiRecord*)precord;
-	/* Max range is 65535 */
-	pRecord->eslo = (pRecord->eguf - pRecord->egul) / 65535;
-	pRecord->roff = 0x0;
-	return 0;
+	aiRecord* prec = static_cast<aiRecord*>(precord);
+
+	prec->dpvt = util::parseAndCreateDpvt(prec->inp.value.instio.string);
+
+	return prec->dpvt ? 0 : 1;
 }
+
+static long el30xx_read_record(void* precord)
+{
+
+}
+
+
+static long el33xx_init_record(void* precord)
+{
+	aiRecord* prec = static_cast<aiRecord*>(precord);
+
+	prec->dpvt = util::parseAndCreateDpvt(prec->inp.value.instio.string);
+
+	return prec->dpvt ? 0 : 1;
+}
+
+static long el33xx_read_record(void* precord)
+{
+
+}
+
+
--- ek9000App/src/drvEK9000.cpp
+++ ek9000App/src/drvEK9000.cpp
@@ -1,40 +1,512 @@
-#include <drvModbusAsyn.h>
-#include <drvAsynIPPort.h>
-#include <asynPortDriver.h>
-#include <modbus.h>
-#include <modbusInterpose.h>
-#include <asynOctetSyncIO.h>
+/*
+ *
+ *	drvEK9000.cpp
+ *
+ * Implementation of simple EK9K driver code
+ *
+ */ 
+/* EPICS includes */
+#include <epicsAtomic.h>
+#include <epicsThread.h>
+#include <epicsSpin.h>
+#include <epicsTime.h>
+#include <epicsStdlib.h>
+#include <epicsPrint.h>
+#include <epicsString.h>
 #include <epicsExport.h>
 #include <iocsh.h>
-#include <epicsString.h> 
+
+/* Modbus includes */
+#include <drvModbusAsyn.h>
+#include <modbus.h>
+#include <modbusInterpose.h>
+
+/* Asyn includes */
+#include <asynInt32.h>
+#include <asynPortClient.h>
+#include <asynOctetSyncIO.h>
+#include <asynPortDriver.h>
+#include <drvAsynIPPort.h>
+
+/* STL Includes */
+#include <time.h>
+#include <unordered_map>
+#include <queue>
+
+#include "drvEK9000.h"
+#include "util.h"
+
+std::vector<drvEK9000*> devices;
+
+/* Quick conversion macro to convert from bytes to modbus registers */
+#define BYTES_TO_REG(x) (((x) % 2) == 0 ? (x) / 2 : ((x) / 2) + 1)
+
+drvEK9000::drvEK9000(const char* ek, const char* port, const char* ipport, const char* ip) :
+	drvModbusAsyn(port, ipport, 0, 2, -1, 65535, dataTypeUInt16, 0, ""),
+	name(ek)
+{
+	this->swapMutex = epicsSpinCreate();
+	this->coeMutex = epicsSpinCreate();
+}
+
+void drvEK9000::StartPollThread()
+{
+	/* Create the name for the thread */
+	char name[128];
+	snprintf(name, 128, "%s_POLLTHREAD", port);
+	
+	this->pollThread = epicsThreadCreate(name, epicsThreadPriorityHigh, epicsThreadStackBig, drvEK9000::PollThreadFunc, (void*)this);
+}
+
+void drvEK9000::PollThreadFunc(void* lparam)
+{
+	const char* function = "drvEK9000::PollThreadFunc";
+	const char* step = "ReadInputRegisters";
+	while(1)
+	{
+		drvEK9000* _this = (drvEK9000*)lparam;
+		asynStatus status;
+		bool oreg = false, obit = false;
+		float dt, coe_dt;
+
+		/* compute the change in time */
+		dt = ((clock()) / (CLOCKS_PER_SEC)/1000.0f) - _this->prevTime;
+
+		/* Check the time change to make sure we have not possibly missed a poll period */
+		if(dt > _this->pollPeriod * 1000.0f)
+		{
+			asynPrint(_this->pasynUserSelf, ASYN_TRACE_WARNING, "%s:%s: Total time between poll calls exceeds poll period! dt=%f, period=%f",
+					function, step, dt, (_this->pollPeriod*1000.0f));
+		}
+
+		asynPrint(_this->pasynUserSelf, ASYN_TRACE_FLOW, "%s: %s\n", function, step);
+
+		/* Read the input registers */
+		step = "ReadInputRegisters";
+		if( _this->doModbusIO(0, MODBUS_READ_INPUT_REGISTERS, INPUT_PDO_START, _this->inputSwapSpace, 120) 				!= asynSuccess ||
+			_this->doModbusIO(0, MODBUS_READ_INPUT_REGISTERS, INPUT_PDO_START + 120, &_this->inputSwapSpace[120], 120)	!= asynSuccess ||
+			_this->doModbusIO(0, MODBUS_READ_INPUT_REGISTERS, INPUT_PDO_START + 240, &_this->inputSwapSpace[240], 15)	!= asynSuccess)
+		{
+			asynPrint(_this->pasynUserSelf, ASYN_TRACE_ERROR, "%s:%s Failed to read input registers, skipping to input bits\n", 
+				function, step);
+		}
+
+		/* Read input bits */
+		step = "ReadInputBits";
+		if( _this->doModbusIO(0, MODBUS_READ_DISCRETE_INPUTS, INPUT_PDO_START, _this->inputBitSwap, 120) 				!= asynSuccess ||
+			_this->doModbusIO(0, MODBUS_READ_DISCRETE_INPUTS, INPUT_PDO_START + 120, &_this->inputBitSwap[120], 120)	!= asynSuccess ||
+			_this->doModbusIO(0, MODBUS_READ_DISCRETE_INPUTS, INPUT_PDO_START + 240, &_this->inputBitSwap[240], 15)		!= asynSuccess)
+		{
+			asynPrint(_this->pasynUserSelf, ASYN_TRACE_ERROR, "%s:%s Failed to read input bits.\n", function, step);
+		}
+
+		/* Trace the flow of the program */
+		step = "SwapRegisters";
+		asynPrint(_this->pasynUserSelf, ASYN_TRACE_FLOW, "%s: %s\n", function, step);
+
+		/* Compare our output swap spaces */
+		epicsSpinLock(_this->swapMutex);
+
+		/* Compare the output PDO and output swap space, and copy it over if they don't match */
+		if(memcmp(_this->outputPDO, _this->outputSwapSpace, OUTPUT_REG_SIZE * sizeof(uint16_t)) == 0)
+		{
+			oreg = true;
+			memcpy(_this->outputSwapSpace, _this->outputPDO, OUTPUT_REG_SIZE * sizeof(uint16_t));
+		}
+
+		/* Same thing as above; compare and swap output bit PDOs */
+		if(memcmp(_this->outputBitPDO, _this->outputBitSwap, OUTPUT_BIT_SIZE * sizeof(uint16_t)) == 0)
+		{
+			obit = false;
+			memcpy(_this->outputBitSwap, _this->outputBitPDO, OUTPUT_BIT_SIZE * sizeof(uint16_t));
+		}
+
+		/* For the input PDO's lets copy in the new data */
+		memcpy(_this->inputPDO, _this->inputSwapSpace, INPUT_REG_SIZE * sizeof(uint16_t));
+		memcpy(_this->inputBitPDO, _this->inputBitSwap, INPUT_BIT_SIZE * sizeof(uint16_t));
+		
+		/* Unlock the swap guard */
+		epicsSpinUnlock(_this->swapMutex);
+		
+		step = "WriteOutputRegisters";
+		asynPrint(_this->pasynUserSelf, ASYN_TRACE_FLOW, "%s: %s\n", function, step);
+
+		/**
+		 * NOTE: modbus TCP can only write/read 123 registers at a time, so that means we need
+		 * to break up the modbus requests into multiple pieces
+		 */ 
+		
+		/* Do I/O for the output registers */
+		step = "WriteOutputRegisters";
+		if( _this->doModbusIO(0, MODBUS_WRITE_MULTIPLE_REGISTERS, OUTPUT_PDO_START, _this->outputSwapSpace, 120) != asynSuccess ||
+			_this->doModbusIO(0 ,MODBUS_WRITE_MULTIPLE_REGISTERS, OUTPUT_PDO_START + 120, &_this->outputSwapSpace[120], 120) != asynSuccess ||
+			_this->doModbusIO(0, MODBUS_WRITE_MULTIPLE_REGISTERS, OUTPUT_PDO_START + 240, &_this->outputSwapSpace[240], 15) != asynSuccess)
+		{
+			asynPrint(_this->pasynUserSelf, ASYN_TRACE_ERROR, "%s:%s Failed to write output registers, skipping to output bits.\n",
+				function, step);
+		}
+
+		/* Do I/O for the coils */
+		step = "WriteOutputCoils";
+		if( _this->doModbusIO(0, MODBUS_WRITE_MULTIPLE_COILS, OUTPUT_COIL_START, _this->outputBitSwap, 120) != asynSuccess ||
+			_this->doModbusIO(0, MODBUS_WRITE_MULTIPLE_COILS, OUTPUT_COIL_START + 120, &_this->outputBitSwap[120], 120) != asynSuccess ||
+			_this->doModbusIO(0, MODBUS_WRITE_MULTIPLE_COILS, OUTPUT_COIL_START + 240, &_this->outputBitSwap[240], 15) != asynSuccess)
+		{
+			asynPrint(_this->pasynUserSelf, ASYN_TRACE_ERROR, "%s:%s Failed to write output bits\n", function, step);
+		}
+
+		/* Set the last time */
+		_this->prevTime = (clock()) / (CLOCKS_PER_SEC/1000.0f);
+
+		step = "DoCoEIO";
+		asynPrint(_this->pasynUserSelf, ASYN_TRACE_FLOW, "%s: %s\n", function, step);
+
+		/* Here we will handle all of the CoE IO requests, as many as possible at least */
+		/* The internal CoE IO function will return a delta t for how long the request took, 
+		 * so we can ensure we don't miss any polling periods */
+		coe_dt = 0.0f;
+
+		while(1)
+		{
+			coe_req_t req;
+			int coe_dt;
+			
+			/* C++ containers are not thread safe :( */
+			epicsSpinLock(_this->coeMutex);
+
+			/* Nothing to do ?? */
+			if(_this->coeRequests.size() <= 0) break;
+			
+			/* Get the next request in the queue */
+			req = _this->coeRequests.front();
+			_this->coeRequests.pop_front();
+			
+			/* Lock our CoE spinlock */
+			epicsSpinUnlock(_this->coeMutex);
+			
+			/* Perform the IO & compute the total delta t */
+			coe_dt += _this->doCoEIO(req);
+
+			/* In the event of us running over the poll period, do not sleep, just rerun the poll! */
+			if(coe_dt >= (_this->pollPeriod * 1000.0f))
+			{
+				asynPrint(_this->pasynUserSelf, ASYN_TRACE_WARNING, "%s: %s: Poll period exceeded for missed due to CoE requests or other latency. coe_dt=%d\n", 
+						function, step, coe_dt);
+				break;
+			}
+
+			/* If within 10ms of the next poll period, it's cutting it kinda close */
+			if(coe_dt >= (_this->pollPeriod * 1000.0f - 10.0f))
+				break;
+		}
+
+		/* Sleep for the poll period minus the dt */
+		epicsThreadSleep(_this->pollPeriod - coe_dt);
+	}
+}
+
+/* Atomically read a register,
+ * type 0 = bo, 1 = bi, 2 = ao, 3 = ai */
+uint16_t drvEK9000::ReadRegisterAtomic(int addr, int type)
+{
+	union {
+		int v;
+		uint16_t i16[2];
+	} ret;
+	ret.v = INT_MAX;
+
+	switch(type)
+	{
+		case 0: ret.v = epicsAtomicGetIntT((int*)&this->outputBitPDO[addr]); break;
+		case 1: ret.v = epicsAtomicGetIntT((int*)&this->inputBitPDO[addr]); break;
+
+		/* For analog output records, we should subtract the start address so we don't overrun the buffer */
+		case 2:
+			addr -= (0x800-1);
+			ret.v = epicsAtomicGetIntT((int*)&this->outputPDO[addr]);
+			break;
+		
+		case 3: ret.v = epicsAtomicGetIntT((int*)&this->inputPDO[addr]); break;
+		default: break;
+	}
+	return ret.i16[0];
+}
+
+/**
+ * Initialize terminal mappings
+ * Coupler must be connected for this to work!
+ * This must also be called from the constructor, and nowhere else,
+ * it's not thread safe
+ */ 
+void drvEK9000::MapTerminals()
+{
+	epicsUInt16 terminalids[0xFF];
+	terminal_t terminals[0xFF];
+
+	/* Starting points of PDOs */
+	int inp_start = 0x1, outp_start = 0x800, inb_start = 0, outb_start = 0;
+
+	/* Read the registers */
+	this->doModbusIO(0, MODBUS_READ_INPUT_REGISTERS, EK9K_SLAVE_MAP_REG, terminalids, 0xFF);
+
+	/* Compute locations in the input/output register space, which will only be analog terms */
+	for(int i = 0; terminalids[i] != 0 && i < 0xFF; i++)
+	{
+		const STerminalInfoConst_t* info = util::FindTerminal(terminalids[i]);
+		if(!info) continue;
+
+		++this->num_terminals;
+
+		/* Set the terminal info */
+		terminals[i].in_start = inp_start;
+		terminals[i].out_start = outp_start;
+		terminals[i].id = terminalids[i];
+		terminals[i].in_size = terminals[i].out_size = terminals[i].inb_size = terminals[i].outb_size = 0;
+
+		/* Determine the type */
+		if(terminalids[i] >= 3000 && terminalids[i] < 4000) terminals[i].type = terminal_t::AI;
+		if(terminalids[i] >= 4000 && terminalids[i] < 5000) terminals[i].type = terminal_t::AO;
+		if(terminalids[i] >= 1000 && terminalids[i] < 2000) terminals[i].type = terminal_t::BI;
+		if(terminalids[i] >= 2000 && terminalids[i] < 3000) terminals[i].type = terminal_t::BO;
+
+		/* Now set the input/output sizes */
+		/* We set them to 0 earlier in this loop */
+		switch(terminals[i].type)
+		{
+			case terminal_t::AI:
+			case terminal_t::AO:
+				terminals[i].in_size = info->m_nInputSize;
+				terminals[i].out_size = info->m_nOutputSize;
+				break;
+			case terminal_t::BI:
+			case terminal_t::BO:
+				terminals[i].inb_size = info->m_nInputSize;
+				terminals[i].outb_size = info->m_nOutputSize;
+				break;
+			default:
+				break;
+		}
+
+		/* Ignore everything less than 3k as we're only doing the analog mapping right now */
+		if(info->m_nID < 3000) continue;
+
+		inp_start += info->m_nInputSize;
+		outp_start += info->m_nOutputSize;
+	}
+
+	/* Do the same for the input bits */
+	for(int i = 0; terminalids[i] != 0 && i < 0xFF; i++)
+	{
+		const STerminalInfoConst_t* info = util::FindTerminal(terminalids[i]);
+		if(!info) continue;
+		if(info->m_nID >= 3000) continue;
+
+		outb_start += info->m_nOutputSize;
+		inb_start += info->m_nInputSize;
+	}
+}
+
+/**
+ * Forcefully performs CoE IO
+ * Not thread-safe
+ * rw = true means write
+ * rw = false means read
+ */ 
+bool drvEK9000::doCoEIO(bool rw, epicsUInt16 termid, epicsUInt16 index, epicsUInt16 subindex, epicsUInt16 bytesize, epicsUInt16* pbuf)
+{
+	assert(bytesize <= 250);
+	assert(pbuf != NULL);
+
+	/* Write */
+	if(rw)
+	{
+		size_t real_size = 0;
+		asynStatus status;
+		epicsUInt16 send_data[0xFF] = {
+			0x1,				/* execute */
+			termid | 0x8000,	/* Termid with bit 0x8000 set for write */
+			subindex ^ 0xFF00,	/* subindex, Clear out the high bits here as they're unused */
+			bytesize,			/* Size in bytes of transfer */
+			0					/* ADS Error code */
+		};
+		/* Copy into the send_data buffer starting with the 6th element */
+		memcpy(&send_data[5], pbuf, bytesize < 250 ? bytesize : 250);
+
+		/* Compute real size of the buffer we're going to send */
+		real_size = 5 + (bytesize/2);
+		real_size = (bytesize % 2 != 0) ? (real_size + 1) : real_size;
+
+		/* Now actually do the IO */
+		status = this->doModbusIO(0, MODBUS_WRITE_MULTIPLE_REGISTERS, REG_0x1400, send_data, real_size);
+
+		return status == asynSuccess;
+	}
+	/* Read */
+	else
+	{
+		asynStatus status;
+		epicsUInt16 status_reg = 0, real_size;
+		/* The initial request */
+		epicsUInt16 send_data[0xFF] = {
+			0x0,				/* just set this to zero for reads */
+			termid ^ 0x8000,	/* bit 15 must be 0 for reading */
+			index,				/* COE index */
+			subindex ^ 0xFF00,	/* Clear high 8 bits */
+			bytesize,
+			0,					/* ADS error code */
+		};
+
+		/* Write out everything */
+		status = this->doModbusIO(0, MODBUS_WRITE_MULTIPLE_REGISTERS, REG_0x1400, send_data, 0x5 /* Number of registers */);
+
+		/* Did not work :( */
+		if(status != asynSuccess) return false;
+
+		/* Compute real size of the buffer we're going to send */
+		real_size = 5 + (bytesize/2);
+		real_size = (bytesize % 2 != 0) ? (real_size + 1) : real_size;
+	
+		/**
+		 * Polling loop
+		 * The ek9k wants us to read 12 registers here for some reason
+		 */ 
+		do 
+		{
+			epicsThreadSleep(0.01);
+			status = this->doModbusIO(0, MODBUS_READ_HOLDING_REGISTERS, REG_0x1400, send_data, real_size);
+			
+			/* safety measure here: break if the read has failed */
+			if(status != asynSuccess) break;
+			
+			status_reg = send_data[0];
+		} while(status_reg & 0x200);
+
+		/* Failure to read if 0x10X is set */
+		if(send_data[0] & 0x100) return false;
+
+		/* Copy over read data */
+		memcpy(pbuf, &send_data[5], bytesize);
+		return true;
+	}
+}
+
+void drvEK9000::DumpInfo()
+{
+}
+
+/* Enqueues a CoE IO request, optionally putting it at the front of the queue */
+void drvEK9000::RequestCoEIO(coe_req_t req, bool immediate)
+{
+	epicsSpinLock(this->coeMutex);
+
+	/* If immediate, put before everything else */
+	if(immediate)
+		this->coeRequests.push_front(req);
+	else
+		this->coeRequests.push_back(req);
+	
+	epicsSpinUnlock(this->coeMutex);
+}
+
+void drvEK9000::RequestCoEIO(coe_req_t* req, int nreq, bool immediate)
+{
+	epicsSpinLock(this->coeMutex);
+
+	for(int i = 0; i < nreq; i++)
+	{
+		if(immediate)
+			this->coeRequests.push_front(req[i]);
+		else
+			this->coeRequests.push_back(req[i]);
+	}
+	
+	epicsSpinUnlock(this->coeMutex);
+}
+
+/**
+ * Basically just rips a coe_req_t structure apart
+ */ 
+bool drvEK9000::doCoEIO(coe_req_t req)
+{
+	return this->doCoEIO(req.type == COE_REQ_WRITE, req.termid, req.index, req.subindex, BYTES_TO_REG(req.length), req.pdata);
+}
+
+void drvEK9000::DumpTerminalMapping()
+{
+	for(int i = 0; i < this->num_terminals; i++)
+	{
+		epicsPrintf("--------------------------------\n");
+		epicsPrintf("#%u: EL%u\n", terminals[i].id);
+		epicsPrintf("Input registers:  0x%X-0x%X\n", terminals[i].in_start);
+		epicsPrintf("Output registers: 0x%X-0x%X\n", terminals[i].out_start);
+		epicsPrintf("Input coils:      0x%X-0x%X\n", terminals[i].inb_start);
+		epicsPrintf("Output coils:     0x%X-0x%X\n", terminals[i].outb_start);
+	}
+}
+
+void drvEK9000::DumpStats()
+{
+
+}
+
+void drvEK9000::DumpInfo()
+{
+
+}
+
+void drvEK9000::DumpEverything()
+{
+	this->DumpInfo();
+	this->DumpStats();
+	this->DumpTerminalMapping();
+}
 
 /*
+================================================================
 
-Driver for the ek9000, just handles queuing and other arbitration
+iocsh functions
 
+================================================================
 */
 
-class drvEK9000 : public drvModbusAsyn
+void ek9000Register(const iocshArgBuf* args)
 {
-private:
+	const char* ek9k = args[0].sval;
+	const char* port = args[1].sval;
+	const char* ip = args[2].sval;
 
-public:
-	drvEK9000(const char* port, const char* octetport);
-	/*
-	Perform a write to a terminal at the specified addr with the given function
-	*/
-	asynStatus writeTerminal(uint16_t addr, uint16_t fn, uint16_t* buf, int len);
+	/* Check if already registered */
+	for(auto x : devices)
+	{
+		if(strcmp(x->name, ek9k) == 0)
+		{
+			epicsStdoutPrintf("Device %s already registered.\n", ek9k);
+			return;
+		}
+	}
 
-};
+	/* Create the octet port name */
+	char octetport[1024]; 
+	snprintf(octetport, 1024, "%s_OCTETPORT", port);
 
-drvEK9000::drvEK9000(const char* port, const char* octet) :
-	drvModbusAsyn(port, octet, 0, 0, 0, 0, dataTypeInt16, 500, "")
+	/* Configure IP port and create EK9K */
+	drvAsynIPPortConfigure(octetport, ip, 0, 0, 0);
+	auto ek = new drvEK9000(ek9k, port, octetport, ip);
+
+	devices.push_back(ek);
+}
+
+void ek9000AddTerminal(const iocshArgBuf* args)
 {
 
 }
 
-asynStatus drvEK9000::writeTerminal(uint16_t addr, uint16_t fn, uint16_t* buf, int len)
+void ek9k_register()
 {
-	
-	return asynSuccess;
+	util::iocshRegister("ek9000Register", ek9000Register,
+			{{"ek9k-name", iocshArgString},
+			{"port", iocshArgString},
+			{"ip", iocshArgString}});
 }
+
+epicsExportRegistrar(ek9k_register);
