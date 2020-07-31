34ef0a9a40c948c208cf19a037dc0149b3dbf2f4
JJL772 <jeremy.lorelli.1337@gmail.com>
2020-07-31 13:03:31 -0700

    Add ekCoE.cpp

---
+++ ek9000App/src/ekCoE.h
@@ -0,0 +1,21 @@
+/*
+ * This file is part of the EK9000 device support module. It is subject to 
+ * the license terms in the LICENSE.txt file found in the top-level directory 
+ * of this distribution and at: 
+ *    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html. 
+ * No part of the EK9000 device support module, including this file, may be 
+ * copied, modified, propagated, or distributed except according to the terms 
+ * contained in the LICENSE.txt file.
+*/
+//======================================================//
+// Name: devEL7XXX.cpp
+// Purpose: Device support for EL7xxx modules (motor control)
+//          requires the motor record module for epics
+// Authors: Jeremy L.
+// Date Created: July 31, 2020
+//======================================================//
+#pragma once
+
+#include <epicsStdlib.h>
+
+
