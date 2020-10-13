ab32a88ae2631ba918e41cb00d3c0c5ab37932ed
Jeremy Lorelli <jeremy.lorelli.1337@gmail.com>
2020-10-13 02:36:10 -0700

    Some QOL updates and fixes

--- ek9000App/src/ekUtil.cpp
+++ ek9000App/src/ekUtil.cpp
@@ -3,7 +3,7 @@
  *
  * Common utilities for use with epics
  *
- */ 
+ */
 #include "ekUtil.h"
 #include <memory.h>
 #include <stdlib.h>
@@ -19,22 +19,22 @@ using namespace util;
 /* This really wont be needed, but it helps us pretend that we aren't leaking memory */
 struct iocshHandles_t
 {
-	iocshArg* args;
-	iocshArg** pargs;
+	iocshArg *args;
+	iocshArg **pargs;
 	iocshFuncDef func;
 };
-std::vector<iocshHandles_t*> functions;
+std::vector<iocshHandles_t *> functions;
 
 /* Register iocsh functions */
-void util::iocshRegister(const char* name, void(*pfn)(const iocshArgBuf*), std::initializer_list<iocshArg> args)
+void util::iocshRegister(const char *name, void (*pfn)(const iocshArgBuf *), std::initializer_list<iocshArg> args)
 {
-	iocshHandles_t* handle = static_cast<iocshHandles_t*>(malloc(sizeof(iocshHandles_t)));
-	handle->args = static_cast<iocshArg*>(malloc(args.size() * sizeof(iocshArg)));
-	handle->pargs = static_cast<iocshArg**>(malloc(args.size() * sizeof(iocshArg*)));
+	iocshHandles_t *handle = static_cast<iocshHandles_t *>(malloc(sizeof(iocshHandles_t)));
+	handle->args = static_cast<iocshArg *>(malloc(args.size() * sizeof(iocshArg)));
+	handle->pargs = static_cast<iocshArg **>(malloc(args.size() * sizeof(iocshArg *)));
 	auto it = args.begin();
-	for(int i = 0; i < args.size() && it; i++, it++)
+	for (int i = 0; i < args.size() && it; i++, it++)
 		handle->args[i] = *it;
-	for(int i = 0; i < args.size(); i++)
+	for (int i = 0; i < args.size(); i++)
 		handle->pargs[i] = &handle->args[i];
 	handle->func = {name, (int)args.size(), handle->pargs};
 	::iocshRegister(&handle->func, pfn);
@@ -43,18 +43,18 @@ void util::iocshRegister(const char* name, void(*pfn)(const iocshArgBuf*), std::
 
 /*
  * @ek9k_name,slave_num,channel
- */ 
-void* util::parseAndCreateDpvt(char* instio)
+ */
+void *util::parseAndCreateDpvt(char *instio)
 {
-	if(!instio) return nullptr;
+	if (!instio)
+		return nullptr;
 	int pindex = 0;
-	
-	for(char* subst = strtok(instio, ","); subst; subst = strtok(NULL, ","), pindex++)
-	{
 
+	for (char *subst = strtok(instio, ","); subst; subst = strtok(NULL, ","), pindex++)
+	{
 	}
 
-	if(pindex < 2)
+	if (pindex < 2)
 	{
 		epicsPrintf("Syntax error in instio string: %s\n", instio);
 		return nullptr;
@@ -62,53 +62,38 @@ void* util::parseAndCreateDpvt(char* instio)
 
 	int ncommas = 0;
 	size_t len = strlen(instio);
-	for(int i = 0; i < len; i++)
+	for (int i = 0; i < len; i++)
 	{
-		
 	}
 }
 
-const STerminalInfoConst_t* util::FindTerminal(int id)
+const STerminalInfoConst_t *util::FindTerminal(int id)
 {
-	for(int i = 0; i < NUM_TERMINALS; i++)
-		if(g_pTerminalInfos[i]->m_nID == id) return g_pTerminalInfos[i];
+	for (int i = 0; i < NUM_TERMINALS; i++)
+		if (g_pTerminalInfos[i]->m_nID == id)
+			return g_pTerminalInfos[i];
 	return nullptr;
 }
 
-void util::Log(const char* fmt, ...)
+void util::Log(const char *fmt, ...)
 {
 	time_t clk = time(0);
 	tm _tm;
-	
+
 	epicsTime_localtime(&clk, &_tm);
 	epicsPrintf("%i:%i ", _tm.tm_hour, _tm.tm_min);
-	
+
 	va_list list;
 	va_start(list, fmt);
 	epicsVprintf(fmt, list);
 	va_end(list);
 }
 
-void util::Warn(const char* fmt, ...)
+void util::Warn(const char *fmt, ...)
 {
 	epicsTimeStamp stmp;
 	epicsTimeGetCurrent(&stmp);
-	
-	char txt[40];
-	epicsTimeToStrftime(txt, 40, "%Y/%m/%d %H:%M:%S.%03f ", &stmp);
-	epicsPrintf("%s", txt);
-	
-	va_list list;
-	va_start(list, fmt);
-	epicsVprintf(fmt, list);
-	va_end(list);
-}
 
-void util::Error(const char* fmt, ...)
-{
-	epicsTimeStamp stmp;
-	epicsTimeGetCurrent(&stmp);
-	
 	char txt[40];
 	epicsTimeToStrftime(txt, 40, "%Y/%m/%d %H:%M:%S.%03f ", &stmp);
 	epicsPrintf("%s", txt);
@@ -119,13 +104,36 @@ void util::Error(const char* fmt, ...)
 	va_end(list);
 }
 
-long util::setupCallback(void* rec, void(*pCallback)(CALLBACK*)) 
+void util::Error(const char *fmt, ...)
 {
-	CALLBACK* callback = (CALLBACK*)calloc(1, sizeof(CALLBACK));
-	*callback = *(CALLBACK*)pCallback;
+	epicsTimeStamp stmp;
+	epicsTimeGetCurrent(&stmp);
+
+	char txt[40];
+	epicsTimeToStrftime(txt, 40, "%Y/%m/%d %H:%M:%S.%03f ", &stmp);
+	epicsPrintf("%s", txt);
+
+	va_list list;
+	va_start(list, fmt);
+	epicsVprintf(fmt, list);
+	va_end(list);
+}
+
+long util::setupCallback(void *rec, void (*pCallback)(CALLBACK *))
+{
+	CALLBACK *callback = (CALLBACK *)calloc(1, sizeof(CALLBACK));
+	*callback = *(CALLBACK *)pCallback;
 	callbackSetCallback(pCallback, callback);
 	callbackSetUser(rec, callback);
 	callbackSetPriority(priorityHigh, callback);
 	callbackRequest(callback);
 	return 0;
+}
+
+terminal_info_t util::getTerminalInfo(uint32_t id)
+{
+}
+
+terminal_info_t util::getTerminalInfo(const char *id)
+{
 }
