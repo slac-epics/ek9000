951142b60dfc520e8be8d8df4ff6c43d939869c8
Jeremy Lorelli <jeremy.lorelli.1337@gmail.com>
2021-04-21 06:45:48 -0700

    feat: EL331X support

--- ek9000App/src/devEL3XXX.cpp
+++ ek9000App/src/devEL3XXX.cpp
@@ -413,3 +413,174 @@ static long EL36XX_linconv(void *precord, int after)
 {
 	return 0;
 }
+
+
+
+//======================================================//
+//
+//	EL331X Device support
+//
+//======================================================//
+static void EL331X_ReadCallback(CALLBACK *callback);
+static long EL331X_dev_report(int interest);
+static long EL331X_init(int after);
+static long EL331X_init_record(void *precord);
+static long EL331X_read_record(void *precord);
+static long EL331X_linconv(void *precord, int after);
+
+struct
+{
+	long number;
+	DEVSUPFUN dev_report;
+	DEVSUPFUN init;
+	DEVSUPFUN init_record;
+	DEVSUPFUN get_ioint_info;
+	DEVSUPFUN read_record;
+	DEVSUPFUN linconv;
+} devEL331X = {
+	6,
+	(DEVSUPFUN)EL331X_dev_report,
+	(DEVSUPFUN)EL331X_init,
+	(DEVSUPFUN)EL331X_init_record,
+	NULL,
+	(DEVSUPFUN)EL331X_read_record,
+	(DEVSUPFUN)EL331X_linconv,
+};
+epicsExportAddress(dset, devEL331X);
+
+struct EL331XDpvt_t
+{
+	devEK9000Terminal *terminal;
+	devEK9000 *device;
+	int channel;
+};
+
+#pragma pack(1)
+struct EL331XInputPDO_t
+{
+	uint32_t inp;
+	uint16_t status;
+};
+#pragma pack()
+
+#define EL36XX_OVERRANGE_MASK 0x2
+#define EL36XX_UNDERRANGE_MASK 0x1
+
+static void EL331X_ReadCallback(CALLBACK *callback)
+{
+	void *record;
+	uint16_t buf[3];
+	EL36XXInputPDO_t* pdo = NULL;
+	static_assert(sizeof(EL36XXInputPDO_t) <= sizeof(buf), "SEL36XXInput is greater than 3 bytes in size! Contact the author regarding this error.");
+
+	callbackGetUser(record, callback);
+	aiRecord *pRecord = static_cast<aiRecord *>(record);
+	EL36XXDpvt_t *dpvt = static_cast<EL36XXDpvt_t *>(pRecord->dpvt);
+	free(callback);
+
+	/* Check for invalid */
+	if (!dpvt->terminal)
+		return;
+
+	/* Lock mutex */
+	int status = dpvt->terminal->m_device->Lock();
+
+	if (status != epicsMutexLockOK)
+	{
+		recGblSetSevr(pRecord, READ_ALARM, INVALID_ALARM);
+		DevError("EL36XX_ReadCallback(): %s\n", devEK9000::ErrorToString(EK_EMUTEXTIMEOUT));
+		return;
+	}
+
+	status = dpvt->terminal->doEK9000IO(MODBUS_READ_INPUT_REGISTERS, dpvt->terminal->m_inputStart + ((dpvt->channel - 1) * 2), buf, 2);
+	pdo = reinterpret_cast<EL36XXInputPDO_t*>(buf);
+	pRecord->rval = pdo->inp;
+
+	/* Check the overrange and underrange flags */
+	if ((pdo->status & EL36XX_OVERRANGE_MASK) || (pdo->status & EL36XX_UNDERRANGE_MASK))
+	{
+		recGblSetSevr(pRecord, READ_ALARM, MAJOR_ALARM);
+	}
+
+	/* Set props */
+	pRecord->pact = FALSE;
+	pRecord->udf = FALSE;
+	dpvt->terminal->m_device->Unlock();
+
+	/* Check for error */
+	if (status)
+	{
+		recGblSetSevr(pRecord, READ_ALARM, INVALID_ALARM);
+		if (status > 0x100)
+		{
+			DevError("EL36XX_ReadCallback(): %s\n", devEK9000::ErrorToString(EK_EMODBUSERR));
+			return;
+		}
+		DevError("EL36XX_ReadCallback(): %s\n", devEK9000::ErrorToString(status));
+		return;
+	}
+	return;
+}
+
+static long EL331X_dev_report(int interest)
+{
+	return 0;
+}
+
+static long EL331X_init(int after)
+{
+	return 0;
+}
+
+static long EL331X_init_record(void *precord)
+{
+	aiRecord *pRecord = static_cast<aiRecord *>(precord);
+	pRecord->dpvt = calloc(1, sizeof(EL36XXDpvt_t));
+	EL36XXDpvt_t *dpvt = static_cast<EL36XXDpvt_t *>(pRecord->dpvt);
+
+	/* Get the terminal */
+	char *recname = NULL;
+	dpvt->terminal = devEK9000Terminal::ProcessRecordName(pRecord->name, dpvt->channel, recname);
+	if (!dpvt->terminal)
+	{
+		Error("EL36XX_init_record(): Unable to find terminal for record %s\n", pRecord->name);
+		return 1;
+	}
+	free(recname);
+
+	dpvt->device = dpvt->terminal->m_device;
+	dpvt->device->Lock();
+
+	/* Check connection to terminal */
+	if (!dpvt->terminal->m_device->VerifyConnection())
+	{
+		Error("EL36XX_init_record(): %s\n", devEK9000::ErrorToString(EK_ENOCONN));
+		dpvt->device->Unlock();
+		return 1;
+	}
+
+	/* Check that slave # is OK */
+	uint16_t termid = 0;
+	dpvt->terminal->m_device->ReadTerminalID(dpvt->terminal->m_terminalIndex, termid);
+	dpvt->device->Unlock();
+
+	/* This is important; if the terminal id is different than what we want, report an error */
+	if (termid != dpvt->terminal->m_terminalId || termid == 0)
+	{
+		Error("EL36XX_init_record(): %s: %s != %u\n", devEK9000::ErrorToString(EK_ETERMIDMIS), pRecord->name, termid);
+		return 1;
+	}
+
+	return 0;
+}
+
+static long EL331X_read_record(void *precord)
+{
+	util::setupCallback(precord, EL36XX_ReadCallback);
+	return 0;
+}
+
+static long EL331X_linconv(void *precord, int after)
+{
+	return 0;
+}
