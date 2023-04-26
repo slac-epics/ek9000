e7eaf5c3f2b4d41aca4b8b28ee073f4085b0dcf7
Jeremy Lorelli <jeremy.lorelli.1337@gmail.com>
2023-04-26 15:04:48 -0700

    MNT: cleanup IOC test scripts
    gen-cmd.py is now the preferred way to generate test cmd files, rather than manually modifying and committing them

---
+++ iocBoot/iocEK9KTest/gen-cmd.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+
+"""
+Generate st.cmd from a list of terminals, or the coupler's modbus configuration
+"""
+
+import os
+import sys
+import re
+import argparse
+
+use_modbus = True
+try:
+	from pymodbus.client import ModbusTcpClient
+except:
+	print('Failed to import pymodbus, terminals must be manually specified on the command line')
+	use_modbus = False
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--ip', type=str, required=True, help='IP address and port the coupler can be reached at')
+parser.add_argument('--out', type=str, default='st.cmd', help='File to output to')
+parser.add_argument('--terminals', type=str, help='Comma separated list of terminals')
+parser.add_argument('--port', type=int, default=502, help='Port to use')
+args = parser.parse_args()
+
+os.chdir(os.path.dirname(__file__))
+
+arch = os.getenv('EPICS_HOST_ARCH')
+if arch is None:
+	print('EPICS_HOST_ARCH environment variable not set')
+	exit(1)
+
+terms = []
+if use_modbus and (args.terminals is None or len(args.terminals) == 0):
+	client = ModbusTcpClient(args.ip, args.port)
+	if not client.connect():
+		print('Connection to coupler failed, use --terminals instead')
+		exit(1)
+	resp = client.read_holding_registers(0x6001, 125)
+	for r in resp.registers:
+		if r == 0:
+			break
+		name = f'EL{r}'
+		if not os.path.exists(f'../../db/{name}.template'):
+			print(f'Unsupported terminal type {name}, stopping here!')
+			break
+		terms.append(f'EL{r}')
+	print(f'Determined layout from coupler: {terms}')
+	client.close()
+else:
+	terms = args.terminals.split(',')
+
+with open('st.tpl', 'r') as fp:
+	data = fp.read()
+	data = data.replace('$ARCH$', arch).replace('$IP$', args.ip).replace('$NUM_TERMS$', str(len(terms))).replace('$PORT$', str(args.port))
+	
+	conf = ''
+	c = 1
+	for t in terms:
+		conf += f'ek9000ConfigureTerminal("EK9K1", "t{c}", "{t}", {c})\n'
+		c += 1
+	data = data.replace('$CONFIGURE$', conf)
+	
+	rec = ''
+	c = 1
+	for t in terms:
+		rec += f'dbLoadRecords("../../db/{t}.template", "TERMINAL=t{c}")\n'
+		c += 1
+	data = data.replace('$RECORDS$', rec)
+	
+	with open(args.out, 'w') as outfp:
+		outfp.write(data)
+	os.chmod(args.out, 0o755)
