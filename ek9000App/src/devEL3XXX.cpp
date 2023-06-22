97cc6218d40a8e016af1a29ca1572b27a03331f1
Jeremy Lorelli <jeremy.lorelli.1337@gmail.com>
2023-06-22 16:20:16 -0700

    FIX: interpret analog input values as int16
    Both signed and unsigned representations behave like a signed 16-bit
    integer. In the case of unsigned, the value is limited to the range
    0-32767, the former is limited to -32768-32767. As such, treating the
    values from the terminal as int16 (and thus sign extending them when
    assigning to RVAL) is the correct way of doing things.
    
    This does not handle the MSB representation properly however. That will
    require more work down the line if we ever decide to support it. For
    now, users will need to use the unsigned or signed representations for
    the data to be correctly read.

--- ek9000App/src/devEL3XXX.cpp
+++ ek9000App/src/devEL3XXX.cpp
@@ -131,7 +131,7 @@ struct EL30XXStandardInputPDO_t {
 	uint8_t _r2 : 6; // Last bit in this align is Sync error for EL31XX
 	uint8_t txpdo_state : 1;
 	uint8_t txpdo_toggle : 1;
-	uint16_t value;
+	int16_t value;	// Must be signed to accommodate bipolar terminals. Unsigned representation still defines range as 0-32767, so this is safe.
 };
 #pragma pack()
 
@@ -339,7 +339,7 @@ struct EL331XInputPDO_t {
 	uint8_t error : 1;
 	uint8_t txpdo_state : 1;
 	uint8_t txpdo_toggle : 1;
-	uint16_t value;
+	int16_t value;
 };
 
 struct EL3314_0010_InputPDO_t {
@@ -351,7 +351,7 @@ struct EL3314_0010_InputPDO_t {
 	uint8_t txpdo_state : 1;
 	uint8_t txpdo_toggle : 1;
 	uint8_t padding1 : 7; // Pad it out to byte boundary
-	uint16_t value;
+	int16_t value;
 };
 #pragma pack()
 
