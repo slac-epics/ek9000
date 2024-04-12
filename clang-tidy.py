890648b23eac31bedb21b9d8a904762010d87880
Jeremy Lorelli <jeremy.lorelli.1337@gmail.com>
2024-04-11 17:01:22 -0700

    ENH: Expand list of clang-tidy checks and use new list syntax
    Bunch of new clang-tidy checks, mostly a subset of bugprone- and misc-.
    Also added annotations explaining why these are necessary.
    
    .clang-tidy now supports a list syntax in clang 17. To retain backwards
    compatibility with older versions of clang-tidy, the script will
    automatically convert the config to a JSON and passes that to clang-tidy
    directly.

--- clang-tidy.py
+++ clang-tidy.py
@@ -3,8 +3,30 @@
 import json
 import subprocess
 import re
+import argparse
+import yaml
+
+def _load_config() -> dict:
+    """
+    Loads the config off of disk. This is required especially for older versions of clang-tidy that do not support the new format, which
+    allows for a nice list of checks instead of a big long comma delimited string
+    """
+    with open('.clang-tidy', 'r') as fp:
+        f = yaml.safe_load(fp)
+        f['Checks'] = ','.join(f['Checks']) # Convert from a list to a comma delimited str
+        return f
+
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--clang-tidy', type=str, dest='clang_tidy', default='clang-tidy', help='Clang tidy executable to use')
+    args = parser.parse_args()
+
+    r = subprocess.run([args.clang_tidy, '--version'], capture_output=True)
+    if r.returncode != 0:
+        print(f'{args.clang_tidy} not found')
+        exit(1)
+
     exclude = []
     with open('.clang-tidy-exclude.json', 'rb') as fp:
         exclude = json.load(fp)
@@ -17,6 +39,8 @@ if __name__ == '__main__':
                 return True
         return False
 
+    conf = json.dumps(_load_config())
+
     returnCode = 0
     with open('compile_commands.json', 'rb') as fp:
         f = json.load(fp)
@@ -24,7 +48,7 @@ if __name__ == '__main__':
             file = cmd['file']
             if not is_excluded(file):
                 print(f'Running clang-tidy on {file}')
-                r = subprocess.run(['clang-tidy', file], cwd=cmd['directory'])
+                r = subprocess.run([args.clang_tidy, f'--config={conf}', file], cwd=cmd['directory'])
                 if r.returncode != 0:
                     returnCode = r.returncode
             else:
